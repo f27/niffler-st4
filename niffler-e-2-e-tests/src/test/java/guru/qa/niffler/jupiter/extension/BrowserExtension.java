a57d2f264027a01071e99eee12e765bfbd1d8135
Farit Aznabaev <fazanbaev@qa.guru>
2023-12-30 14:07:12 +0500

    homework 2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/BrowserExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/BrowserExtension.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 import java.io.ByteArrayInputStream;
+import java.util.Objects;
 
 
 public class BrowserExtension implements AfterEachCallback, TestExecutionExceptionHandler {
@@ -18,9 +18,9 @@ public class BrowserExtension implements AfterEachCallback, TestExecutionExcepti
   public void handleTestExecutionException(ExtensionContext extensionContext, Throwable throwable) throws Throwable {
     if (WebDriverRunner.hasWebDriverStarted()) {
       Allure.addAttachment(
-          "Screen after test",
+          "Screenshot on fail",
           new ByteArrayInputStream(
-              ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES)
+                  Objects.requireNonNull(Selenide.screenshot(OutputType.BYTES))
           )
       );
     }
